d23b557c3e74f005e2ae1d3a67da5ddde6beac85
S. Makai Takori <makait87@gmail.com>
2019-12-19 04:16:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   if num > 1
     range = (n..num-1).to_a 
     range.none? do |number| 
-      number % n == 0
+      num % number == 0
     end  
   else
     false
@@ -13,15 +13,3 @@ end
 
 # The none?() of enumerable is an inbuilt method in Ruby returns a boolean value true if none of the objects in the enumerable satisfies the given condition, else it returns false
 
-def prime?(number)
-  start = 2
-  if number > 1
-    range = (start..number-1).to_a
-    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
-                                # return true, then they are not divisible by the number and the number is prime
-      number % num_to_test == 0
-    end
-  else
-    false
-  end
-end
