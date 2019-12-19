42b83c8f222e28d1c736b6e0e7f92b835260d323
S. Makai Takori <makait87@gmail.com>
2019-12-19 03:29:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,18 @@
-def prime?(num)
-  n = 2       #prime numbers are not divisible by 2 
-  while n < num 
+def is_prime(num)
+  (2..(num - 1)).each do |n|
     return false if num % n == 0
-    n += 1  
   end
   true
-end  
+end
+
+
+
+
+# def prime?(num)
+#   n = 2       #prime numbers are not divisible by 2 
+#   while n < num 
+#     return false if num % n == 0
+#     n += 1  
+#   end
+#   true
+# end  
