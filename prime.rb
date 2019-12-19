874b36db2c3a66cf20e24680552c9a2fc6e0edbb
S. Makai Takori <makait87@gmail.com>
2019-12-19 04:13:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,9 +6,10 @@ def prime?(num)
       number % n == 0
     end  
   else
-   return false
+    false
   end 
 end  
 
 
 # The none?() of enumerable is an inbuilt method in Ruby returns a boolean value true if none of the objects in the enumerable satisfies the given condition, else it returns false
+
