15d2973b7194726007b45c0fdf84f1414330f6e5
S. Makai Takori <makait87@gmail.com>
2019-12-19 03:58:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,8 @@
-# def prime?(num)
-#   (2..(num - 1)).each do |n|
-#     return false if num.abs % n == 0
-#   end
-#   true
-# end
-
-
-
-
 def prime?(num)
   n = 2       #prime numbers are not divisible by 2 
-  while n < num.abs 
-    return false if num.abs % n == 0
-    n += 1  
-  end
-  true
+  
+  if num % n == 0 
+    return false  
+  else
+    true
 end  
